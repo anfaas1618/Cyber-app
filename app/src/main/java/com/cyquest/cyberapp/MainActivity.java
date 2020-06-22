7a1e57387f6b2bd48804b610dbac1a1ea6e67890
anfaas1618 <ishad0911@gmail.com>
2020-06-22 12:28:31 +0530

    added 3 cards func

--- app/src/main/java/com/cyquest/cyberapp/MainActivity.java
+++ app/src/main/java/com/cyquest/cyberapp/MainActivity.java
@@ -3,12 +3,39 @@ package com.cyquest.cyberapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+ImageView imageView1,imageView2,imageView3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView1=findViewById(R.id.imageView1);
+        imageView2=findViewById(R.id.imageView2);
+        imageView3=findViewById(R.id.imageView3);
+                imageView1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                       Toast.makeText(MainActivity.this, "first", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                imageView2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "second", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                imageView3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "third", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
+
+
 }
